5ce9c3abf451194d1fcede8576b7490370fbbbcd
vaishnavi2022121 <vaishnavikhandapanor@gmail.com>
2026-07-20 17:26:01 +0530

    Add myinfo.sh script to display system information

---
+++ myinfo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "My Name: Vaishnavi"
+echo "Current User:"
+whoami
+
+echo "Current Directory:"
+pwd
+
+echo "Today's Date:"
+date
+
+echo "Hostname:"
+hostname
+
+echo "Linux Version:"
+uname -a
